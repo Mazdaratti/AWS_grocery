0003395f09375c2ae16b59cc77d79ecc9402c0a5
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-03-03 11:11:11 +0100

    Updated variables

--- Infrastructure/variables.tf
+++ Infrastructure/variables.tf
@@ -205,7 +205,7 @@ variable "restrict_public_buckets" {
   type        = bool
   default     = false
 }
-variable "prefix" {
+variable "avatar_prefix" {
   description = "The prefix for the avatars storage path"
   type        = string
   default     = "avatars/"
