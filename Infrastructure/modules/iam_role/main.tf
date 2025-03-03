25d3e8459428959a32de7cc4b0ffb886ab75f892
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-03-03 19:20:56 +0100

    Improved readability

--- Infrastructure/modules/iam_role/main.tf
+++ Infrastructure/modules/iam_role/main.tf
@@ -2,20 +2,18 @@
 resource "aws_iam_role" "ec2_role" {
   name = var.iam_role_name
 
-  assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
       {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "ec2.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
       }
     ]
-  }
-  EOF
+  })
 }
 
 resource "aws_iam_policy" "ec2_s3_access_policy" {
